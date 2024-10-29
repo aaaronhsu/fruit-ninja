b11d86678df2870caece433f57ca829acd6b5120
Aaron Hsu <aaron.hsu@duke.edu>
2024-10-29 15:43:44 -0400

    backend

---
+++ backend/server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+import psycopg2
+app = Flask(__name__)
+
+load_dotenv()
+
+database_user = os.getenv("DB_USER")
+database_password = os.getenv("DB_PASSWORD")
+database_name = os.getenv("DB_NAME")
+
+CREATE_TABLE = """
+CREATE TABLE IF NOT EXISTS test (
+    id SERIAL PRIMARY KEY,
+    points INT
+);
+"""
+
+conn = psycopg2.connect(
+    dbname=database_name,
+    user=database_user,
+    password=database_password,
+    host='localhost'
+)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Fruit Ninja!</p>"
+
+@app.post("/api/create_table")
+def create_table():
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(CREATE_TABLE)
+    return {"message": "Table Created."}, 201
