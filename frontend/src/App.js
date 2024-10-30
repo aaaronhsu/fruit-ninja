9ac822d91ad175aedd4dcaedb22ec10b6b7492ae
Aaron Hsu <aaron.hsu@duke.edu>
2024-10-29 23:12:42 -0400

    frontend socket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import io from "socket.io-client";
+import CreateTableButton from "./components/CreateTableButton";
+import UpdatePointButton from "./components/UpdatePointButton";
+import React, { useEffect, useState } from "react";
 
-import CreateTableButton from "./components/button";
+const socket = io("http://ec2-34-195-221-35.compute-1.amazonaws.com:8000");
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    socket.on("point_update", (data) => {
+      setData(data);
+    });
+  });
+
   return (
     <div className="App">
       <h1>AHHHHH</h1>
       <CreateTableButton />
+
+      <UpdatePointButton />
+      {data && <div>{JSON.stringify(data)}</div>}
     </div>
   );
 }
