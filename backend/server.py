743517c13909d6e685102b5e52325f96e14b6661
Aaron Hsu <aaron.hsu@duke.edu>
2024-11-05 23:01:35 -0500

    parse input

--- backend/server.py
+++ backend/server.py
@@ -48,9 +48,9 @@ def game_events():
 
             match event['type']:
                 case GameEvent.FRUIT_SLICED:
-                    dao.update_points(event['game_id'], 1)
+                    dao.update_points(event['game_id'], event['metadata']['points'])
                 case GameEvent.BOMB_SLICED:
-                    dao.update_lives(event['game_id'], -1)
+                    dao.update_lives(event['game_id'], -event['metadata']['damage'])
                 case GameEvent.GAME_END:
                     dao.end_game(event['game_id'])
 
